f9ca46d43d01a42088639e8aef1a99a82daad8f6
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2025-04-13 19:36:13 +0900

    [#7] 검색 화면 UI 개발 - 홈화면 UI 브랜치에서 공통 UI 요소 가져오기

---
+++ core/src/main/java/project/side/ikdaman/core/ui/Color.kt
@@ -0,0 +1,11 @@
+package project.side.ikdaman.core.ui
+
+import androidx.compose.ui.graphics.Color
+
+object Palette {
+    val first = Color(0xFFC1A4DB)
+    val second = Color(0xFF4C8ED9)
+    val third = Color(0xFF26843B)
+    val fourth = Color(0xFF694E4E)
+    val fifth = Color(0xFF595959)
+}
