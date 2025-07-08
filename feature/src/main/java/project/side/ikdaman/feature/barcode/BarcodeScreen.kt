752ccfd5c6761d9fc71f81e18a73e43db6354371
Hyegyeong <63198157+whk06061@users.noreply.github.com>
2025-07-09 00:05:41 +0900

    [#16] 사용하지 않는 import 제거

--- feature/src/main/java/project/side/ikdaman/feature/barcode/BarcodeScreen.kt
+++ feature/src/main/java/project/side/ikdaman/feature/barcode/BarcodeScreen.kt
@@ -32,7 +32,6 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SheetState
