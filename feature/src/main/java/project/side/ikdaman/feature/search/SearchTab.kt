dad5469c92f95634543d161a43f6838dcfb9f667
whk06061 <gju0606@naver.com>
2025-09-07 18:27:55 +0900

    feat: 책 검색 화면 - 정보와 버튼이 겹치는 문제 수정

--- feature/src/main/java/project/side/ikdaman/feature/search/SearchTab.kt
+++ feature/src/main/java/project/side/ikdaman/feature/search/SearchTab.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -33,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -308,42 +311,46 @@ private fun SearchResultItem(
     bookItem: BookItem,
     onClickAddBookButton: (String) -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 15.dp)
-    ) {
-        Row {
-            AsyncImage(
-                model = bookItem.cover,
-                contentDescription = "Book Cover",
-                modifier = Modifier
-                    .size(width = 80.dp, height = 114.dp)
+    Row(modifier = Modifier.height(IntrinsicSize.Max)) {
+        AsyncImage(
+            model = bookItem.cover,
+            contentDescription = "Book Cover",
+            modifier = Modifier
+                .size(width = 80.dp, height = 114.dp)
+                .align(Alignment.CenterVertically),
+            placeholder = ColorPainter(Color.Gray),
+            error = ColorPainter(Color.Red)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(start = 18.dp)
+        ) {
+            Text(
+                text = bookItem.title,
+                style = TextStyle(
+                    fontFamily = PretendardFontFamily,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp
+                ),
+                modifier = Modifier.padding(bottom = 3.dp)
             )
-            Column(modifier = Modifier.padding(start = 18.dp)) {
-                Text(
-                    text = bookItem.title,
-                    style = TextStyle(
-                        fontFamily = PretendardFontFamily,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 14.sp
-                    ),
-                    modifier = Modifier.padding(bottom = 3.dp)
+            Text(
+                text = bookItem.author,
+                style = TextStyle(
+                    fontFamily = PretendardFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 12.sp
                 )
-                Text(
-                    text = bookItem.author,
-                    style = TextStyle(
-                        fontFamily = PretendardFontFamily,
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 12.sp
-                    )
+            )
+            Spacer(Modifier.weight(1f))
+            Row(Modifier.fillMaxWidth()) {
+                Spacer(Modifier.weight(1f))
+                AddBookButton(
+                    onClick = { onClickAddBookButton(bookItem.isbn) }
                 )
             }
         }
-        AddBookButton(
-            onClick = { onClickAddBookButton(bookItem.isbn) },
-            modifier = Modifier.align(Alignment.BottomEnd)
-        )
     }
 }
 
